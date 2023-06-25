447590b412cb01b07b3a1475db66b985c94eef18
Axel <axel.deb.len@gmail.com>
2023-06-25 11:58:37 +1000

    initial commit

---
+++ sanity/README.md
@@ -0,0 +1,1 @@
+run `npm create sanity@latest init --env` to get started
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import '../app.css';
+</script>
+
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
+</style>
